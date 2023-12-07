feb1228638af040feaf43bc6acc5635a94fed49e
yelo-o <flyordig@gmail.com>
2023-12-07 22:13:27 +0900

    Login 기능 구현

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <script setup>
+import { Nav, Alert } from '@/components';
+import { useAuthStore } from '@/stores';
+
+const authStore = useAuthStore();
 </script>
 
 <template>
-  <div>
-
+  <div class="app-container" :class="authStore.user && 'bg-light'">
+      <Nav />
+      <Alert />
+      <div class="container pt-4 pb-4">
+          <router-view />
+      </div>
   </div>
 </template>
 
